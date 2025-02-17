6485acad8be74557511a8de80ccda77f44fb253b
Victor Coura <victor.mcoura@hotmail.com>
2025-02-17 15:37:11 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+namespace revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de números ");
+            int tamanhoLista  = Convert.ToInt32(Console.ReadLine());
+            int[] lista = new int[tamanhoLista];
+
+
+            for (int i = 0; i < tamanhoLista; i++)
+            {
+                Console.WriteLine($"Digite um número: ");
+                lista[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            for (int i = (tamanhoLista - 1); i >= 0; i--)
+            {   
+                Console.Write("Sua Lista de Números é: ");
+                Console.WriteLine(lista[i]);
+            }
+
+
+
+        }
+    }
+
+}
+
+
+
